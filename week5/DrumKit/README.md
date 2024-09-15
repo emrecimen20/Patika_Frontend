b4936ddb3b6795ae3e3d3799428559d521c24816
emrecimen20 <emrecimen48@outlook.com>
2024-09-15 14:23:02 +0300

    Add drum kit project

---
+++ week5/DrumKit/README.md
@@ -0,0 +1,14 @@
+# Drum Application
+
+This project is a web application that allows you to play various drum sounds. Users can trigger these sounds by either clicking on buttons or pressing specific keyboard keys.
+
+## Features
+
+- **Button-Based Sound Playback**: Click on buttons on the screen to play different drum sounds.
+- **Keyboard Shortcuts**: Press specific keys on the keyboard to play corresponding drum sounds.
+
+## Installation
+
+To get the project up and running, follow these steps:
+
+
---
+++ week5/DrumKit/drum.js
@@ -0,0 +1,106 @@
+document.getElementById("btn-1").addEventListener("click", () => {
+    document.getElementById("au-1").play();
+});
+
+document.addEventListener("keydown", (event)=> {
+    
+    if (event.key === "a" || event.key === "A") {
+        document.getElementById("au-1").play();
+    }
+});
+
+
+document.getElementById("btn-2").addEventListener("click", ()=>{
+    document.getElementById("au-2").play();
+});
+
+document.addEventListener("keydown", (event)=> {
+    
+    if (event.key === "s" || event.key === "S") {
+        document.getElementById("au-2").play();
+    }
+});
+
+
+document.getElementById("btn-3").addEventListener("click", ()=>{
+    document.getElementById("au-3").play();
+});
+
+document.addEventListener("keydown", (event)=> {
+    
+    if (event.key === "d" || event.key === "D") {
+        document.getElementById("au-3").play();
+    }
+});
+
+
+document.getElementById("btn-4").addEventListener("click", ()=>{
+    document.getElementById("au-4").play();
+});
+4
+document.addEventListener("keydown", (event)=> {
+    
+    if (event.key === "f" || event.key === "F") {
+        document.getElementById("au-4").play();
+    }
+});
+
+
+document.getElementById("btn-5").addEventListener("click", ()=>{
+    document.getElementById("au-5").play();
+});
+
+document.addEventListener("keydown", (event)=> {
+    
+    if (event.key === "g" || event.key === "G") {
+        document.getElementById("au-5").play();
+    }
+});
+
+
+document.getElementById("btn-6").addEventListener("click", ()=>{
+    document.getElementById("au-6").play();
+});
+
+document.addEventListener("keydown", (event)=> {
+    
+    if (event.key === "h" || event.key === "H") {
+        document.getElementById("au-6").play();
+    }
+});
+
+
+document.getElementById("btn-7").addEventListener("click", ()=>{
+    document.getElementById("au-7").play();
+});
+
+document.addEventListener("keydown", (event)=> {
+    
+    if (event.key === "j" || event.key === "J") {
+        document.getElementById("au-7").play();
+    }
+});
+
+
+document.getElementById("btn-8").addEventListener("click", ()=>{
+    document.getElementById("au-8").play();
+});
+
+document.addEventListener("keydown", (event)=> {
+    
+    if (event.key === "k" || event.key === "K") {
+        document.getElementById("au-8").play();
+    }
+});
+
+
+document.getElementById("btn-9").addEventListener("click", ()=>{
+    document.getElementById("au-9").play();
+});
+
+document.addEventListener("keydown", (event)=> {
+    
+    if (event.key === "l" || event.key === "L") {
+        document.getElementById("au-9").play();
+    }
+});
